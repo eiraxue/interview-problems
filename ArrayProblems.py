2226fe94c0b8afd8454e725dc6c91d3c165c5a3a
eiraxue <eirabenxue@gmail.com>
2016-09-29 23:42:14 -0400

    Update ArrayProblems.py

--- ArrayProblems.py
+++ ArrayProblems.py
@@ -40,3 +40,41 @@ def findMaxSumSequence(inputList):
     
 testList = [-4, 2, 3, 0, -1, 10, 2, -3]
 findMaxSumSequence(testList)
+
+#%%
+# find if the list has the target number
+def checkIfExist(inputList, x):
+    N=len(inputList)
+    if N==1:
+        if x!= inputList[0]:
+            return false
+        else:
+            return true
+    if x < inputList[N/2]:
+        checkIfExist(inputList[:N/2-1], x)
+    elif x > inputList[N/2]:
+        checkIfExist(inputList[N/2+1:], x)
+    else:
+        return true
+    
+testList = [-9, -3, 1, 3, 4, 8]
+print checkIfExist(testList, -9)  
+
+
+#%%
+# find if the list has the target number, plain implementation
+def checkIfExist(inputList, x):
+    low = 0
+    high = len(inputList)-1
+    while(low <= high):
+        mid = (low+high)/2
+        if x<inputList[mid]:
+            high = mid-1
+        elif x>inputList[mid]:
+            low = mid+1
+        else:
+            return mid
+    return -1
+            
+testList = [-9, -3, 1, 3, 4, 8]
+print checkIfExist(testList, 7)  
